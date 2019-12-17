6df5c500ab84d56dc0a0a7f17365a55ea86a6f2a
h-ARTS <hi5hanan@yahoo.com>
2019-12-17 16:59:01 +0100

    API lockdown, cors, body-parser and implementation of login, signup

--- api/__test__/server.spec.js
+++ api/__test__/server.spec.js
@@ -1,15 +1,36 @@
 import request from 'supertest'
 import { app } from '../server'
+import { User } from '../resources/user/user.model'
+import { newToken } from '../utils/auth'
 import mongoose from 'mongoose'
 
 describe('API Authentication:', () => {
   let token
   beforeEach(async () => {
-    // TODO: Create new user and token
+    const user = await User.create({
+      email: 'max@mustard.com',
+      password: '123456',
+      fullName: 'Dr. Max Mustard',
+      handle: '@DrMustard'
   })
-  describe('/api auth', () => {
-    test('should be locked and throws 401 Unauthorized', async () => {})
+    token = newToken(user)
+  })
 
-    test('passes with JWT', async () => {})
+  describe('/api auth', () => {
+    test('should be locked down and throws 401 Unauthorized', async () => {
+      expect.assertions(4)
+
+      let response = await request(app).get('/api/user')
+      expect(response.statusCode).toBe(401)
+
+      response = await request(app).get('/api/tweet')
+      expect(response.statusCode).toBe(401)
+
+      response = await request(app).get('/api/tweets')
+      expect(response.statusCode).toBe(401)
+
+      response = await request(app).get('/api/notifications')
+      expect(response.statusCode).toBe(401)
+    })
   })
 })
