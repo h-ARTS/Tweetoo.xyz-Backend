3fcbe55a92437ba147e11cde6633121bc81d9ea7
h-ARTS <hi5hanan@yahoo.com>
2019-12-29 20:49:22 +0100

    useless comments

--- api/resources/tweet/tweet.controllers.js
+++ api/resources/tweet/tweet.controllers.js
@@ -1,13 +1,4 @@
 import { controllers } from '../../utils/crud'
 import { Tweet } from './tweet.model'
 
-// TODO: Create Like Model first
-// controllers(Tweet).likeTweet = async (req, res) => {
-//   try {
-//     const likedTweet = this.findOneAndUpdate({_id: req.params.tweetId}, {likeCount: req.body.likeCount+1})
-
-//     if (!likedTweet)
-//   }
-// }
-
 export default controllers(Tweet)
