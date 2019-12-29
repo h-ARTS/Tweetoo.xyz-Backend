6e3b6bed72cb466630c8227d739dc407d4a5a55a
h-ARTS <hi5hanan@yahoo.com>
2019-12-29 23:08:23 +0100

    watcher added for deletion of tweet
    - deletion of likes and replies

--- api/resources/tweet/tweet.controllers.js
+++ api/resources/tweet/tweet.controllers.js
@@ -1,4 +1,25 @@
 import { controllers } from '../../utils/crud'
 import { Tweet } from './tweet.model'
+import { Like } from '../like/like.model'
+import { Reply } from '../reply/reply.model'
+
+// Watcher works only on mongodb replica sets
+const watchTweets = Tweet.watch()
+
+watchTweets.on('change', async result => {
+  if (result.operationType === 'delete') {
+    const tweet = result.documentKey
+
+    await Like.find()
+      .where('tweetId')
+      .all(tweet._id)
+      .remove()
+
+    await Reply.find()
+      .where('tweetId')
+      .all(tweet._id)
+      .remove()
+  }
+})
 
 export default controllers(Tweet)
