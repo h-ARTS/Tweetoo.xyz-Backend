cdb1dbc71cf9ca414e6b0ff0f102ecb598e5fe95
h-ARTS <hi5hanan@yahoo.com>
2020-01-28 20:26:45 +0100

    checkUserAssets test pushed back to the controller test file

--- api/resources/user/user-assets/__tests__/assets.controller.spec.js
+++ api/resources/user/user-assets/__tests__/assets.controller.spec.js
@@ -1,7 +1,11 @@
 import fs from 'fs'
-import { removeFileRecursive, removeFile } from '../assets.controller'
+import {
+  removeFileRecursive,
+  removeFile,
+  checkUserAssets
+} from '../assets.controller'
 
-describe('User assets controller:', () => {
+describe('User assets-controller:', () => {
   describe('removeFileRecursive:', () => {
     const reqUserHandle = 'JohnDoe'
     const path = `media/${reqUserHandle}`
@@ -30,6 +34,26 @@ describe('User assets controller:', () => {
       })
 
       expect(fs.existsSync(path)).toBe(false)
+    })
+  })
+
+  describe('checkUserAssets:', () => {
+    test('calls a function to remove users asset folder if found.', () => {
+      const req = {
+        user: {
+          handle: 'Kilza'
+        }
+      }
+      const next = () => {}
+
+      fs.mkdir('media/Kilza', err => {
+        if (err) console.error(err)
+        console.log('media/Kilza created for mock function')
+
+        checkUserAssets(req, {}, next)
+
+        expect(fs.existsSync(`/media/${req.user.handle}`)).toBe(false)
+      })
     })
   })
 })
