39da72897e12c4eeec527dfba8ba6da516e84fb4
h-ARTS <hi5hanan@yahoo.com>
2020-02-01 12:24:11 +0100

    Removed code for notifying after new reply using the changeStream
    - Creates a lot of inconsistencies
    - Not readable

--- api/resources/reply/reply.controllers.js
+++ api/resources/reply/reply.controllers.js
@@ -2,12 +2,13 @@ import { controllers } from '../../utils/crud'
 import { Reply } from './reply.model'
 import { Like } from '../like/like.model'
 
-const watchReplies = Reply.watch()
+const watchReplies = Reply.watch({
+  fullDocument: 'updateLookup'
+})
 
-watchReplies.on('change', async result => {
-  if (result.operationType === 'delete') {
-    const reply = result.documentKey
-
+watchReplies.on('change', async change => {
+  const reply = change.fullDocument
+  if (change.operationType === 'delete') {
     await Like.find()
       .where('docId')
       .all([reply._id])
