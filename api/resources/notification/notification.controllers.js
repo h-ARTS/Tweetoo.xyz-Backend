aa021bdfe42923377719a5423ae9df2518724dd8
h-ARTS <hi5hanan@yahoo.com>
2020-02-06 21:59:37 +0100

    notification getter controller with test

--- api/resources/notification/notification.controllers.js
+++ api/resources/notification/notification.controllers.js
@@ -1,11 +1,14 @@
-import { Notification } from './notification.router'
+import { Notification } from './notification.model'
 
 export const getNotifications = async (req, res) => {
   try {
-    const notifications = await Notification.find({ recipient: req.user._id })
+    const notifications = await Notification.find({
+      recipient: req.user.handle
+    })
 
     res.status(200).json({ notifications })
   } catch (e) {
+    console.error(e)
     res.status(404).send(e)
   }
 }
