5e92a9a3b1a56cd4c68f6d30cb599529a833c593
h-ARTS <hi5hanan@yahoo.com>
2020-03-27 23:56:31 +0100

    port change

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const privateKeyPath = path.resolve('.ssh', process.env.PRIVATE_KEY_FILE)
 const publicKeyPath = path.resolve('.ssh', process.env.PUBLIC_KEY_FILE)
 
 const config = {
-  port: 3000,
+  port: 6500,
   secrets: {
     privateKey: fs.readFileSync(privateKeyPath, 'utf8'),
     publicKey: fs.readFileSync(publicKeyPath, 'utf8'),
