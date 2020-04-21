a4162c2d965a0b9d282ffc8d4c7f2e0a92bbfcd5
h-ARTS <hi5hanan@yahoo.com>
2020-04-21 20:43:02 +0200

    migration uploadImage.js

--- api/utils/uploadImage.ts
+++ api/utils/uploadImage.ts
@@ -1,37 +1,38 @@
-import fs from 'fs'
-import multer from 'multer'
+import * as fs from 'fs'
+import * as multer from 'multer'
+import { Request } from 'express'
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    const targetPath =
+const storage: multer.StorageEngine = multer.diskStorage({
+  destination: (req: Request, file: Express.Multer.File, 
+    cb: (error: Error | null, destination: string) => void): void => {
+    const targetPath: string =
       !req.params || req.params.handle !== ''
         ? req.params.type === 'newuser'
           ? './media/cached/'
           : `./media/user/${req.params.handle}/`
         : `./media/tweet/${req.params.tweetId}/`
+
     return fs.mkdir(targetPath, { recursive: true }, () => {
       cb(null, targetPath)
     })
   },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + file.originalname)
+  filename: (req: Request, file: Express.Multer.File, 
+    cb: (error: Error | null, filename: string) => void): void => {
+      cb(null, Date.now() + file.originalname)
   }
 })
 
-const fileFilter = (req, file, cb) => {
+const fileFilter = (req: Request, file: Express.Multer.File, 
+  cb: multer.FileFilterCallback): void => {
   if (
     file.mimetype === 'image/jpeg' ||
     file.mimetype === 'image/png' ||
     file.mimetype === 'image/gif' ||
     file.mimetype === 'image/webp'
   ) {
-    console.log(file)
     return cb(null, true)
   } else {
-    return cb(
-      new Error(`File type ${file.mimetype} not allowed to upload.`),
-      false
-    )
+    return cb(new Error(`File type ${file.mimetype} not allowed to upload.`))
   }
 }
 
