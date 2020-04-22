4a9f3363162696cfc28ac0d19b64bf93d5e49886
h-ARTS <hi5hanan@yahoo.com>
2020-04-22 20:31:21 +0200

    miragtion assets.controller.js

--- api/resources/user/user-assets/assets.controller.ts
+++ api/resources/user/user-assets/assets.controller.ts
@@ -1,11 +1,14 @@
-import fs from 'fs'
+import * as fs from 'fs'
+import { IRequestUser } from '../../../utils/auth'
+import { NextFunction, Response } from 'express'
 
-export const removeFileRecursive = (path, callback) => {
-  fs.readdir(path, (_, files) => {
+export const removeFileRecursive = 
+  (path: string, callback: () => void): void => {
+  fs.readdir(path, (_, files: string[]): void => {
     files.forEach(file => {
       const currentPath = `${path}/${file}`
       if (fs.lstatSync(currentPath).isDirectory()) {
-        removeFileRecursive(currentPath)
+        removeFileRecursive(currentPath, null)
       } else {
         fs.unlinkSync(currentPath)
       }
@@ -15,14 +18,15 @@ export const removeFileRecursive = (path, callback) => {
   callback()
 }
 
-export const removeFile = path => {
+export const removeFile = (path: string): void => {
   return fs.unlink(path, err => {
     if (err) throw err
     console.log(`${path} is deleted!`)
   })
 }
 
-export const checkUserAssets = (req, _, next) => {
+export const checkUserAssets = (req: IRequestUser, res: Response, 
+  next: NextFunction): void => {
   const path = `media/user/${req.user.handle}`
   if (fs.existsSync(path)) {
     return removeFileRecursive(path, () => {
