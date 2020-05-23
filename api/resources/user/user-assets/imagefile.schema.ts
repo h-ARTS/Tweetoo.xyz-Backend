db69987d8f50a6a6de46420a710f2fb14f574c47
h-ARTS <hi5hanan@yahoo.com>
2020-05-23 11:23:56 +0200

    typo in imagefile schema for default images

--- api/resources/user/user-assets/imagefile.schema.ts
+++ api/resources/user/user-assets/imagefile.schema.ts
@@ -1,5 +1,4 @@
 import { Schema, Document } from 'mongoose'
-
 export interface IImageFile extends Document {
   name: String,
   type: string,
@@ -11,7 +10,7 @@ export const ImageFileSchema = new Schema({
   name: {
     type: String,
     required: true,
-    default: 'no_cover.js'
+    default: 'no_cover.jpg'
   },
   type: {
     type: String,
@@ -21,7 +20,7 @@ export const ImageFileSchema = new Schema({
   url: {
     type: String,
     required: true,
-    default: 'media/standard/no_cover.js'
+    default: 'media/standard/no_cover.jpg'
   },
   mediaId: {
     type: String,
